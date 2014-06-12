arfbtwn/hyena

commit 4f76fab004b053f5219a36ade9df6ff4e99f3bcb

ColumnCellText: Render text with RenderLayout instead of using Cairo
This should make the text follow the GTK theme.

Hyena.Gui/Hyena.Data.Gui/ColumnCellText.cs
@@ -106,15 +106,8 @@ namespace Hyena.Data.Gui
                 return;
             }
 
-            //context.Context.Rectangle (0, 0, cellWidth, cellHeight);
-            //context.Context.Clip ();
-            context.Context.MoveTo (Padding.Left, ((int)cellHeight - text_height) / 2);
-            var color = CairoExtensions.GdkRGBAToCairoColor (context.Theme.Widget.StyleContext.GetColor (context.State));
-            color.A = Alpha ?? (context.Opaque ? 1.0 : 0.5);
-            context.Context.SetSourceColor (color);
-
-            Pango.CairoHelper.ShowLayout (context.Context, context.Layout);
-            //context.Context.ResetClip ();
+            int y_pos = ((int)cellHeight - text_height) / 2;
+            context.Widget.StyleContext.RenderLayout (context.Context, Padding.Left, y_pos, context.Layout);
         }
 
         public void UpdateText (CellContext context, double cellWidth)

commit 295c3f1bb56a434fc773175ab32bd1c2bf9f7aad

ColumnCellText: Set the correct StateFlags on the StyleContext
This ensures that the text is in the right color when the cell is
selected.

This fixes a regression introduced in commit 4f76fab0, which
causes the cell state to be ignored when drawing with RenderLayout.

Hyena.Gui/Hyena.Data.Gui/ColumnCellText.cs
@@ -107,6 +107,7 @@ namespace Hyena.Data.Gui
             }
 
             int y_pos = ((int)cellHeight - text_height) / 2;
+            context.StyleContext.State = context.State;
             context.StyleContext.RenderLayout (context.Context, Padding.Left, y_pos, context.Layout);
         }
 